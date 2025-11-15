ad8ea365461c219531765e73e4b60de187da0496
Apple <apple@Apples-MacBook-Pro-3.local>
2025-11-15 20:59:25 +0000

    school server set up

--- config/config.php
+++ config/config.php
@@ -1,18 +1,17 @@
 <?php
-// Database configuration
+// School server database configuration
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'book_management');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_NAME', '2370207_book_management'); // School server format
+define('DB_USER', '2370207');
+define('DB_PASS', ''); // Your school MySQL password
 
-// Application settings
-define('BASE_URL', 'http://localhost/book_management_system/');
+// School server URL
+define('BASE_URL', 'http://mi-linux.wlv.ac.uk/~2370207/book_management_system/');
 define('SITE_NAME', 'Book Management System');
 
-// Error reporting (disable in production)
+// Error reporting (might need to disable on school server)
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-// Session configuration
 session_start();
 ?>
